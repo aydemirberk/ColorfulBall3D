ab16700757624a93060fff1e1b636b539efcaa6d
aydemirberk <beerkaydemir@gmail.com>
2022-07-29 23:43:09 +0300

    Added Save For Sound and Vibration Preferences
    Added save function for sound and vibration buttons. For next game and sets player's preferences from last game

--- Colorful Ball 3D/Assets/Scripts/UIManager.cs
+++ Colorful Ball 3D/Assets/Scripts/UIManager.cs
@@ -19,6 +19,18 @@ public class UIManager : MonoBehaviour
     public GameObject iap;
     public GameObject information;
 
+    public void Start()
+    {
+        if (PlayerPrefs.HasKey("Sound") == false)
+        {
+            PlayerPrefs.SetInt("Sound", 1);
+        }
+
+        if(PlayerPrefs.HasKey("Vibration") == false)
+        {
+            PlayerPrefs.SetInt("Vibration", 1);
+        }
+    }
 
     //Buton Functions
 
@@ -28,13 +40,40 @@ public class UIManager : MonoBehaviour
         {
             settingsButtonSituation = true;
             layoutAnimator.SetTrigger("Slide_in");
-            
+
         }
 
         else if (settingsButtonSituation == true)
         {
             settingsButtonSituation = false;
             layoutAnimator.SetTrigger("Slide_out");
+        }
+
+        if (PlayerPrefs.GetInt("Sound") == 1) //If Sound is on
+        {
+            soundOn.SetActive(true);
+            soundOff.SetActive(false);
+            AudioListener.volume = 1;
+        }
+
+        else if (PlayerPrefs.GetInt("Sound") == 2) //If sound is off
+        {
+            soundOn.SetActive(false);
+            soundOff.SetActive(true);
+            AudioListener.volume = 0;
+        }
+
+
+        if (PlayerPrefs.GetInt("Vibration") == 1) // If vibration is on
+        {
+            vibrationOn.SetActive(true);
+            vibrationOff.SetActive(false);
+        }
+
+        else if (PlayerPrefs.GetInt("Vibration") == 2) // If vibration is off
+        {
+            vibrationOn.SetActive(false);
+            vibrationOff.SetActive(true);
         }
     }
 
@@ -43,6 +82,7 @@ public class UIManager : MonoBehaviour
         soundOn.SetActive(false);
         soundOff.SetActive(true);
         AudioListener.volume = 0;
+        PlayerPrefs.SetInt("Sound", 2);
     }
 
     public void SoundOff()
@@ -50,20 +90,23 @@ public class UIManager : MonoBehaviour
         soundOff.SetActive(false);
         soundOn.SetActive(true);
         AudioListener.volume = 1;
-
+        PlayerPrefs.SetInt("Sound", 1);
     }
 
     public void VibrationOn()
     {
         vibrationOn.SetActive(false);
         vibrationOff.SetActive(true);
+        PlayerPrefs.SetInt("Vibration", 2);
     }
 
     public void VibrationOff()
     {
         vibrationOff.SetActive(false);
         vibrationOn.SetActive(true);
+        PlayerPrefs.SetInt("Vibration", 1);
     }
+
     public IEnumerator WhiteEffect()
     {
         whiteEffectImage.gameObject.SetActive(true);
